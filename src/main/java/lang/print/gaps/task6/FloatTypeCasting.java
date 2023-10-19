3cb5cca76080fe866b88dbbfde24f7c20e87a955
8ar7ar <5ar7ar@mail.ru>
2023-10-19 22:52:55 +0500

    stage0-module3-task6 "1st attempt" "locally tested"

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        if (numberToBeRounded % 1 < 0.5){
+            int result = (int) Math.floor(numberToBeRounded);
+            System.out.println(result);
+        } else {
+            int result = (int) Math.ceil(numberToBeRounded);
+            System.out.println(result);
+        }
     }
 }
